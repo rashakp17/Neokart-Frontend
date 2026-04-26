c06cc0adcf24bb557cb42a43ffaea0187383067f
marshook ali <marshookali98@gmail.com>
2026-04-26 14:37:32 +0530

    Initial Next.js setup (App Router + Tailwind)

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="p-10">
       <h1 className="text-3xl font-bold">Heedy Project</h1>
-      <p className="mt-4">Frontend setup is ready </p>
+      <p className="mt-4">Frontend setup is ready 🚀</p>
     </main>
   );
 }
